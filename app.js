8fc6ac51f039b2f1978df18c90bfab21b4e7db04
yenyee912 <fuyazi1997@gmail.com>
2021-06-14 01:34:11 +0800

    v1.0.2
    - added time out
    - added helmet

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var mqtt = require('mqtt');
 var express = require('express')
 const methodOverride = require("method-override");
-
+var timeout = require('connect-timeout')
+var helmet = require('helmet')
 var app = express();
 var sleep = require('system-sleep');
 
@@ -30,10 +31,17 @@ var corsMiddleware = function (req, res, next) {
 };
 app.use(corsMiddleware);
 app.use(methodOverride("_method"));
+app.use(helmet())
 require("dotenv").config();
 
+// app.use(timeout('5s'))
+// app.use(haltOnTimedout)
+function haltOnTimedout(req, res, next) {
+  if (!req.timedout) next()
+}
+
 var options = {
-  clientId: "mqttjs_e9cf61b2",
+  mqttClientId: "mqttjs_e9cf61b2",
   // username:"",
   // password:"",
   clean: true
@@ -42,106 +50,163 @@ var options = {
 //mqtt topic
 var topicPrefix = `/sbase/`
 
-app.get('/api/v1/status', async (req, res) => {
-  var client = mqtt.connect(`mqtt://${process.env.HOST}`, options)
-  let locationName = req.query.location
+/*{
+   2 quite bad, operationa, 4 is infornational
+
+ { "name": "pod1", "events": 1, "level": 3, "type": "MQTT", "status": "MQTT=1", "msg": "connected to MQTT" }
+} */
+
+function checkIsQueryValid(podName, location) {
+  let nameSubStr = podName.substring(0, 3)
+  let podNumber = podName.substring(3, podName.length + 1)
+  console.log(nameSubStr, podNumber)
+  if (podName != null && location != null && nameSubStr == 'pod' && parseInt(podNumber) >= 1 && parseInt(podNumber) <= 16)
+    return true
+  else return false
+}
+
+app.get('/api/v1/status', timeout('5s'), haltOnTimedout, async (req, res) => {
+  var mqttClient = mqtt.connect(`mqtt://${process.env.HOST}`, options)
+  const locationName = req.query.location
+  const podName = req.query.name
 
   let statusTopic = topicPrefix + locationName + '/status'
   let pubTopic = topicPrefix + locationName + '/cmd'
 
   try {
-    client.on('connect', () => {
+    let isValid = checkIsQueryValid(podName, locationName)
 
-      client.subscribe(statusTopic)
-      client.publish(pubTopic, JSON.stringify({ name: req.query.name, STATUS: 1 }))
-    })
+    if (isValid == true) {
+      mqttClient.on('connect', () => {
+        mqttClient.subscribe(statusTopic)
+        mqttClient.publish(pubTopic, JSON.stringify({ name: podName, STATUS: 1 }))
+      })
 
-    client.on('message', (topic, message) => {
+      mqttClient.on('message', (topic, message) => {
+        let encodedMsg = JSON.parse(message)
+        if (topic == statusTopic && encodedMsg.name == podName, encodedMsg.type == 'status') {
+          console.log(JSON.parse(message))
+          res.status(200).send(JSON.parse(message))
+        }
 
-      res.status(200).send(JSON.parse(message))
-      client.end()
-    })
+      })
+
+      req.on("close", function () {
+        // console.log('req.close is here')
+        mqttClient.end();
+      });
+
+      req.on("end", function () {
+        // console.log('req.end is here')
+        mqttClient.end();
+      });
+    }
+
+    else {
+      mqttClient.on('connect', () => {
+        mqttClient.subscribe(statusTopic)
+        mqttClient.publish(pubTopic, JSON.stringify({ name: req.query.name, STATUS: 1 }))
+      })
+      res.status(400).send('Unexpected query. Please check the pod name and location.')
+    }
   }
 
   catch (err) {
-    console.log(err.message)
+    res.status(500).send(err.message)
   }
 
 })
 
-app.get('/api/v1/network', async (req, res) => {
-  var client = mqtt.connect(`mqtt://${process.env.HOST}`, options)
+app.get('/api/v1/network', timeout('5s'), haltOnTimedout, async (req, res) => {
+  var mqttClient = mqtt.connect(`mqtt://${process.env.HOST}`, options)
   let locationName = req.query.location
+  let podName = req.query.name
 
   let statusTopic = topicPrefix + locationName + '/status'
   let pubTopic = topicPrefix + locationName + '/cmd'
 
   try {
-    client.on('connect', () => {
+    let isValid = checkIsQueryValid(podName, locationName)
 
-      client.subscribe(statusTopic)
-      client.publish(pubTopic, JSON.stringify({ name: req.query.name, NETWORK: 1 }))
-    })
+    if (isValid == true) {
+      mqttClient.on('connect', () => {
 
-    client.on('message', (topic, message) => {
+        mqttClient.subscribe(statusTopic)
+        mqttClient.publish(pubTopic, JSON.stringify({ name: req.query.name, NETWORK: 1 }))
+      })
 
-      res.status(200).send(JSON.parse(message))
-      client.end()
-    })
+      mqttClient.on('message', (topic, message) => {
+
+        res.status(200).send(JSON.parse(message))
+        mqttClient.end()
+      })
+    }
+
+    else {
+      res.status(400).send('Unexpected query. Please check the pod name and location.')
+    }
   }
 
   catch (err) {
-    console.log(err.message)
+    res.status(500).send(err.message)
   }
 })
 
 app.post('/api/v1/reset', async (req, res) => {
-  var client = mqtt.connect(`mqtt://${process.env.HOST}`, options)
+  var mqttClient = mqtt.connect(`mqtt://${process.env.HOST}`, options)
 
   let locationName = req.query.location
   let pubTopic = topicPrefix + locationName + '/cmd'
 
   try {
-    client.on('connect', () => {
+    mqttClient.on('connect', () => {
 
-      client.publish(pubTopic, JSON.stringify({ name: req.query.name, RESET: 1 }))
+      mqttClient.publish(pubTopic, JSON.stringify({ name: req.query.name, RESET: 1 }))
 
-      sleep(1000)
-      res.send('ok')
+      sleep(3500)
+      res.status(200).send('ok')
 
-      client.end()
+      mqttClient.end()
 
     })
   }
 
   catch (err) {
-    console.log(err.message)
+    res.status(500).send(err.message)
   }
 })
 
 app.post('/api/v1/cmd', async (req, res) => {
-  var client = mqtt.connect(`mqtt://${process.env.HOST}`, options)
+  var mqttClient = mqtt.connect(`mqtt://${process.env.HOST}`, options)
 
   let locationName = req.query.location
   let pubTopic = topicPrefix + locationName + '/cmd'
-  let subTopic = topicPrefix + locationName + '/status'
+  // let subTopic = topicPrefix + locationName + '/status'
 
   let cmds = req.body
   cmds.name = req.query.name
 
   try {
-    client.on('connect', () => {
-      client.subscribe(subTopic)
+    let isValid = checkIsQueryValid(req.query.name, req.query.location)
 
-      client.publish(pubTopic, JSON.stringify(cmds))
+    if (isValid == true) {
+      mqttClient.on('connect', () => {
+        // mqttClient.subscribe(subTopic)
 
-      res.send('ok')
-      client.end()
-    })
+        mqttClient.publish(pubTopic, JSON.stringify(cmds))
+
+        res.status(200).send('ok')
+        mqttClient.end()
+      })
+    }
+
+    else {
+      res.send('wrong query')
+    }
   }
 
   catch (err) {
-    console.log(err.message)
+    res.status(500).send(err.message)
   }
 })
 
